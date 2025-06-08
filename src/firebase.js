bd8e19dc8b74f03fa797698c7351327c3c5ff89b
Hakan Ertürk <133115008+Hakanertrk@users.noreply.github.com>
2025-06-08 17:33:35 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYYyRMkQmwYcAPxBUS3WOaS7dWykT-d8A",
-  authDomain: "smart-to-do-app-ab860.firebaseapp.com",
-  projectId: "smart-to-do-app-ab860",
-  storageBucket: "smart-to-do-app-ab860.firebasestorage.app",
-  messagingSenderId: "687535278528",
-  appId: "1:687535278528:web:26795de07f95ccd83de025",
-  measurementId: "G-CY3G5CX7R6"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
